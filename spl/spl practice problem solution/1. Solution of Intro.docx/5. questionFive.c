b1ee060cc5d51f060e875ac92da950fa52a6cbc4
MD IFTYKHAR WAHID IFTY <mewahid01@gmail.com>
2025-11-14 10:56:51 +0600

    spl - spl practice problem solution - 1. Solution of Intro.docx (1 to 5)

---
+++ spl/spl practice problem solution/1. Solution of Intro.docx/5. questionFive.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    // Program that will do the followings:
+
+
+    // a)	Declare a variable uninitialized
+    int a;
+
+    // b)	Declare and  initialize a variable in one statement
+    int b = 10;
+
+    // c)	Declare and initialize multiple variables with different values in one statement
+    int c1 = 10, c2 = 20, c3 = 30;
+
+    // d)	Declare and initialize multiple variables with the same value in one statement
+    int d1 = d2 = d3 = 50;
+
+    return 0;
+}
